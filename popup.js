9cb2f690a361a48c96e971d28cc0fc4431ef1bd1
reyt0m <reytom1715@gmail.com>
2023-05-07 11:53:22 +0900

    optimize

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('startButton').addEventListener('click', async () => {
-	const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-	chrome.scripting.executeScript({
-	  target: { tabId: tab.id },
-	  files: ['content_script.js']
-	});
-  });
